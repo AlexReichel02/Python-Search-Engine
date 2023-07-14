3e26a6799b90e1734fd200a045c93e498e277481
Alex Reichel <97651259+AlexReichel02@users.noreply.github.com>
2023-07-14 10:34:12 -0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+SEARCH_KEY = ""
+SEARCH_ID = "e0e0e25bd522b4d42"
+COUNTRY = "us"
+
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
+RESULT_COUNT = 20
